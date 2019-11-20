a272abaae3fe80541ab67a848627ccfe968f1620
FrancisFire <34342660+FrancisFire@users.noreply.github.com>
2019-11-20 22:10:05 +0100

    [ADD] Inizializzata codebase

---
+++ Backend/src/main/java/com/github/francisfire/anavis/AnAvisApplication.java
@@ -0,0 +1,13 @@
+package com.github.francisfire.anavis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnAvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnAvisApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/github/francisfire/anavis/AnAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.francisfire.anavis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnAvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
