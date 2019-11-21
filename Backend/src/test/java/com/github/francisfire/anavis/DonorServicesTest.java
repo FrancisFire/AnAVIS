4ca14a19f34a1c58bedc5a7b8f6ad95b0d41db3c
azzeccagarbugli <f.coppola1998@gmail.com>
2019-11-22 00:05:41 +0100

    [UPDATE] Aggiornate dipendenze di Gradle integrando JUnit5

--- Backend/src/test/java/com/github/francisfire/anavis/DonorServicesTest.java
+++ Backend/src/test/java/com/github/francisfire/anavis/DonorServicesTest.java
@@ -5,11 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import com.github.francisfire.anavis.models.Donatore;
 import com.github.francisfire.anavis.models.UfficioAVIS;
 import com.github.francisfire.anavis.services.GestoreDonatori;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class DonorServicesTest {
 
 	private static GestoreDonatori donorServices;
