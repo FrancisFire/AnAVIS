5cc630293b40186a77beb45a2ba6d8b5f4c32a14
azzeccagarbugli <f.coppola1998@gmail.com>
2019-11-22 14:18:43 +0100

    [UPDATE] Prima implementazione del frontend secondo i canoni descritti dai casi d'uso

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
 
 void main() => runApp(Homepage());
 
@@ -13,24 +14,42 @@ class _HomepageState extends State<Homepage> {
     return MaterialApp(
       theme: ThemeData(
         fontFamily: 'Rubik',
-        primaryColor: Colors.red,
+        primaryColor: Colors.white,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("AnAVIS"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Main page del progetto',
-              )
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "AnAVIS",
+              style: TextStyle(
+                color: Colors.red,
+              ),
+            ),
+            bottom: TabBar(
+              labelColor: Colors.red,
+              indicatorColor: Colors.redAccent,
+              labelPadding: EdgeInsets.all(12.0),
+              tabs: <Widget>[
+                buildTextTabBar("Donatore AVIS"),
+                buildTextTabBar("Cittadino"),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.face),
+              Icon(Icons.face),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Text buildTextTabBar(String message) {
+    return Text(
+      message,
     );
   }
 }
