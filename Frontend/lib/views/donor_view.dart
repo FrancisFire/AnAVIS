99b5487f42c17861dc3f9415aa14ae672e0a4b69
azzeccagarbugli <f.coppola1998@gmail.com>
2019-12-15 17:14:53 +0100

    [UPDATE] Aggiornata visualizzazione FAB all'interno della pagina relativa al Donor

--- Frontend/lib/views/donor_view.dart
+++ Frontend/lib/views/donor_view.dart
@@ -6,6 +6,7 @@ import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:provider/provider.dart';
@@ -166,82 +167,101 @@ class _DonorViewState extends State<DonorView> {
               ],
             ),
           ),
-          FabCircularMenu(
-            child: Container(
-              child: Center(
-                child: Stack(
-                  children: <Widget>[
-                    Positioned.fill(
-                      top: (MediaQuery.of(context).size.height / 4),
-                      bottom: 8,
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Swiper(
-                          itemBuilder: (BuildContext context, int index) {
-                            return Card(
-                              color: Colors.white,
-                              elevation: 7,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(26.0),
+          Container(
+            child: Center(
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    top: (MediaQuery.of(context).size.height / 4),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Swiper(
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                            color: Colors.white,
+                            elevation: 7,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(26.0),
+                              ),
+                            ),
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  "Possiamo mostrare delle cose qui che possono essere un grafico o un calendario",
+                                  textAlign: TextAlign.center,
                                 ),
                               ),
-                              child: Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16.0),
-                                  child: Text(
-                                    "Possiamo mostrare delle cose qui che possono essere un grafico o un calendario",
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                          itemCount: 10,
-                          itemWidth: 330.0,
-                          itemHeight:
-                              (MediaQuery.of(context).size.height / 1.8),
-                          layout: SwiperLayout.STACK,
-                        ),
+                            ),
+                          );
+                        },
+                        itemCount: 10,
+                        itemWidth: 330.0,
+                        itemHeight: (MediaQuery.of(context).size.height / 1.7),
+                        layout: SwiperLayout.STACK,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            ringColor: Colors.red,
-            fabColor: Colors.red,
-            fabOpenIcon: Icon(
-              FontAwesomeIcons.tint,
-            ),
-            ringDiameter: MediaQuery.of(context).size.width * 1.1,
-            options: iconFAB(),
           ),
         ],
+      ),
+      floatingActionButton: SpeedDial(
+        marginRight: 18,
+        marginBottom: 20,
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size: 22.0),
+        visible: true,
+        closeManually: false,
+        curve: Curves.bounceIn,
+        overlayColor: Colors.white,
+        overlayOpacity: 0.6,
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+        elevation: 8.0,
+        shape: CircleBorder(),
+        children: iconFAB(),
       ),
     );
   }
 
-  List<Widget> iconFAB() {
-    return <Widget>[
-      BuildRaisedButtonFAB(
-        icon: Icon(
-          Icons.done_outline,
+  List<SpeedDialChild> iconFAB() {
+    return <SpeedDialChild>[
+      SpeedDialChild(
+        child: Icon(
+          Icons.done,
           color: Colors.white,
         ),
-        onPressed: () {
+        onTap: () {
           Navigator.pushNamed(
             context,
             '/donor/candonate',
           );
         },
+        label: 'Possibilità di donare',
+        labelBackgroundColor: Colors.redAccent,
+        backgroundColor: Colors.redAccent,
+        labelStyle: TextStyle(
+          fontSize: 18.0,
+          color: Colors.white,
+        ),
       ),
-      BuildRaisedButtonFAB(
-        icon: Icon(
+      SpeedDialChild(
+        child: Icon(
           Icons.add,
           color: Colors.white,
         ),
-        onPressed: () {
+        label: 'Richiesta di donazione',
+        labelBackgroundColor: Colors.redAccent,
+        backgroundColor: Colors.redAccent,
+        labelStyle: TextStyle(
+          fontSize: 18.0,
+          color: Colors.white,
+        ),
+        onTap: () {
           if (_donorCanDonate) {
             Navigator.pushNamed(
               context,
@@ -256,54 +276,81 @@ class _DonorViewState extends State<DonorView> {
           }
         },
       ),
-      Badge(
-        showBadge: getPrenotationCount() > 0 ? true : false,
-        badgeContent: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Text(getPrenotationCount().toString()),
+      SpeedDialChild(
+        label: 'Lista di prenotazioni',
+        labelBackgroundColor: Colors.redAccent,
+        backgroundColor: Colors.redAccent,
+        labelStyle: TextStyle(
+          fontSize: 18.0,
+          color: Colors.white,
         ),
-        position: BadgePosition.topRight(right: 1, top: 2),
-        badgeColor: Colors.white,
-        child: BuildRaisedButtonFAB(
-          icon: Icon(
-            Icons.receipt,
-            color: Colors.white,
+        child: Center(
+          child: Badge(
+            toAnimate: false,
+            showBadge: getPrenotationCount() > 0 ? true : false,
+            badgeContent: Padding(
+              padding: const EdgeInsets.all(1.4),
+              child: Text(getPrenotationCount().toString()),
+            ),
+            position: BadgePosition.topRight(top: -9, right: -2),
+            badgeColor: Colors.white,
+            child: Icon(
+              Icons.receipt,
+              color: Colors.white,
+            ),
           ),
-          onPressed: () {
-            Navigator.pushNamed(
-              context,
-              '/donor/prenotationsview',
-            );
-          },
         ),
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            '/donor/prenotationsview',
+          );
+        },
       ),
-      Badge(
-        showBadge: getPendingCount() > 0 ? true : false,
-        badgeContent: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Text(getPendingCount().toString()),
+      SpeedDialChild(
+        label: 'Richieste in attesa',
+        labelBackgroundColor: Colors.redAccent,
+        backgroundColor: Colors.redAccent,
+        labelStyle: TextStyle(
+          fontSize: 18.0,
+          color: Colors.white,
         ),
-        position: BadgePosition.topRight(right: 1, top: 2),
-        badgeColor: Colors.white,
-        child: BuildRaisedButtonFAB(
-          icon: Icon(
-            Icons.calendar_today,
-            color: Colors.white,
+        child: Center(
+          child: Badge(
+            toAnimate: false,
+            showBadge: getPendingCount() > 0 ? true : false,
+            badgeContent: Padding(
+              padding: const EdgeInsets.all(1.4),
+              child: Text(getPendingCount().toString()),
+            ),
+            position: BadgePosition.topRight(top: -9, right: -2),
+            badgeColor: Colors.white,
+            child: Icon(
+              Icons.calendar_today,
+              color: Colors.white,
+            ),
           ),
-          onPressed: () {
-            Navigator.pushNamed(
-              context,
-              '/donor/pendingprenotationsview',
-            );
-          },
         ),
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            '/donor/pendingprenotationsview',
+          );
+        },
       ),
-      BuildRaisedButtonFAB(
-        icon: Icon(
+      SpeedDialChild(
+        label: 'Profilo',
+        labelBackgroundColor: Colors.redAccent,
+        backgroundColor: Colors.redAccent,
+        labelStyle: TextStyle(
+          fontSize: 18.0,
+          color: Colors.white,
+        ),
+        child: Icon(
           Icons.account_circle,
           color: Colors.white,
         ),
-        onPressed: () {},
+        onTap: () {},
       ),
     ];
   }
