17fe6a9093e01029dcc54b4288e39d5004b175a8
ZamponiMarco <zamponi.marco3@gmail.com>
2020-01-09 11:33:42 +0100

    [UPDATE] Aggiunta configurazione security funzionante

--- Backend/src/main/java/com/github/francisfire/anavis/config/SecurityConfig.java
+++ Backend/src/main/java/com/github/francisfire/anavis/config/SecurityConfig.java
@@ -4,31 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/api/office/**").hasRole("OFFICE").antMatchers("/api/donor/**")
-				.hasRole("DONOR").and().httpBasic().and().logout().permitAll();
-	
+	    http.sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().httpBasic();
 	}
-
+	
 	@Autowired
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("donor").password(encoder().encode("password")).roles("DONOR").and()
 				.withUser("office").password(encoder().encode("password")).roles("OFFICE");
 	}
-
+	
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
 }
--- Backend/src/main/java/com/github/francisfire/anavis/controller/DonorController.java
+++ Backend/src/main/java/com/github/francisfire/anavis/controller/DonorController.java
@@ -3,6 +3,7 @@ package com.github.francisfire.anavis.controller;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class DonorController {
 	@Autowired
 	private DonorServices donorServices;
 
+	@PreAuthorize("hasRole('DONOR')")
 	@GetMapping("/{donorId}/canDonate")
 	public boolean checkDonationPossibility(@PathVariable("donorId") String donorId) {
 		return donorServices.checkDonationPossibility(donorId);
