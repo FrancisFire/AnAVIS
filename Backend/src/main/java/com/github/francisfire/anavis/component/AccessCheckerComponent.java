d611d2f0a5d257fdbd35a0763b187f9f834cfc70
ZamponiMarco <zamponi.marco3@gmail.com>
2020-01-20 16:23:05 +0100

    [UPDATE] Aggiunta autorizzazione DonationController

--- Backend/src/main/java/com/github/francisfire/anavis/component/AccessCheckerComponent.java
+++ Backend/src/main/java/com/github/francisfire/anavis/component/AccessCheckerComponent.java
@@ -1,13 +1,28 @@
 package com.github.francisfire.anavis.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import com.github.francisfire.anavis.models.ClosedPrenotation;
+import com.github.francisfire.anavis.services.DonationServices;
 
 @Component
 public class AccessCheckerComponent {
 
+	@Autowired
+	private DonationServices donationServices;
+
 	public boolean sameUserId(UserDetails principal, String userId) {
 		return principal.getUsername().equals(userId);
 	}
-	
+
+	public boolean isDonationOwnedById(UserDetails principal, String donationId) {
+		ClosedPrenotation prenotation = donationServices.getDonationInstance(donationId);
+		if (prenotation == null) {
+			return false;
+		}
+		return principal.getUsername().equals(prenotation.getDonorMail());
+	}
+
 }
--- Backend/src/main/java/com/github/francisfire/anavis/controller/DonationController.java
+++ Backend/src/main/java/com/github/francisfire/anavis/controller/DonationController.java
@@ -3,11 +3,13 @@ package com.github.francisfire.anavis.controller;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.francisfire.anavis.component.AccessCheckerComponent;
 import com.github.francisfire.anavis.models.ClosedPrenotation;
 import com.github.francisfire.anavis.models.DonationReport;
 import com.github.francisfire.anavis.services.DonationReportServices;
@@ -19,14 +21,21 @@ public class DonationController {
 
 	@Autowired
 	private DonationServices donationServices;
+	
 	@Autowired
 	private DonationReportServices closedPrenotationReportServices;
+	
+	@SuppressWarnings("unused")
+	@Autowired
+	private AccessCheckerComponent accessCheckerComponent;
 
+	@PreAuthorize("@accessCheckerComponent.sameUserId(principal, #donorMail)")
 	@GetMapping("/donor/{donorMail}")
 	public Set<ClosedPrenotation> getDonationsByDonor(@PathVariable("donorMail") String donorMail) {
 		return donationServices.getDonationsByDonor(donorMail);
 	}
 	
+	@PreAuthorize("@accessCheckerComponent.isDonationOwnedById(principal, #donationId)")
 	@GetMapping("/report/{donationId}")
 	public DonationReport getDonationReport(@PathVariable("donationId") String donationId) {
 		String reportId = donationServices.getDonationInstance(donationId).getReportId();
