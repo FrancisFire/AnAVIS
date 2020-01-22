37fe6b42fb29821ef216666b91b6b2db358d6bc8
azzeccagarbugli <f.coppola1998@gmail.com>
2020-01-22 18:52:36 +0100

    [UPDATE] Miglioramento visivo della pagina e del widget relativo al caricamento

--- Frontend/lib/views/widgets/loading_circular.dart
+++ Frontend/lib/views/widgets/loading_circular.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
 
 class RequestCircularLoading extends StatelessWidget {
   const RequestCircularLoading({
@@ -7,10 +9,71 @@ class RequestCircularLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: CircularProgressIndicator(
-          strokeWidth: 5,
+    return Scaffold(
+      body: Container(
+        color: Colors.redAccent,
+        child: Stack(
+          children: <Widget>[
+            WaveWidget(
+              config: CustomConfig(
+                colors: [
+                  Colors.red[900],
+                  Colors.red[600],
+                  Colors.red[400],
+                  Colors.red[300],
+                ],
+                durations: [
+                  35000,
+                  19440,
+                  10800,
+                  6000,
+                ],
+                heightPercentages: [0.20, 0.23, 0.25, 0.50],
+                blur: MaskFilter.blur(
+                  BlurStyle.solid,
+                  12,
+                ),
+              ),
+              waveAmplitude: 0,
+              size: Size(MediaQuery.of(context).size.width,
+                  MediaQuery.of(context).size.height),
+            ),
+            Positioned(
+              right: 24,
+              left: 24,
+              top: MediaQuery.of(context).size.height / 2.5,
+              child: Center(
+                child: Card(
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(18.0),
+                    ),
+                  ),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.all(12),
+                    leading: Icon(
+                      Icons.cached,
+                      size: 32,
+                      color: Colors.red,
+                    ),
+                    title: Text(
+                      "Caricamento in corso",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "Si prega di attendere il completamento del caricamento della pagina",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
