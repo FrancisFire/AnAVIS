8fcfd4c54a07f6bd72ac7db86667fdc939bd44bc
azzeccagarbugli <f.coppola1998@gmail.com>
2020-01-26 19:37:55 +0100

    [UPDATE] Aggiunti pulsanti per la visualizzazione delle FAQ e della normativa per la privacy all'interno della schermata di login

--- Frontend/lib/views/login_view.dart
+++ Frontend/lib/views/login_view.dart
@@ -59,55 +59,175 @@ class _LoginViewState extends State<LoginView> {
       ),
     );
     return Scaffold(
-      body: FlutterLogin(
-        theme: LoginTheme(
-          primaryColor: Colors.red,
-          accentColor: Colors.red[600],
-          buttonStyle: TextStyle(
-            fontWeight: FontWeight.w800,
-            color: Colors.white,
-          ),
-          errorColor: Colors.deepOrange,
-          titleStyle: TextStyle(
-            fontWeight: FontWeight.w800,
-            fontSize: 48,
-            color: Colors.white,
-            letterSpacing: 2,
-          ),
-        ),
-        title: 'AnAVIS',
-        onLogin: _authUser,
-        logo: 'assets/images/homepage_login.png',
-        onSignup: (LoginData loginData) {
-          Navigator.of(context).pushReplacementNamed(
-            '/guest/createuser',
-            arguments: GuestCreateDonorArgs(
-              loginData.name,
-              loginData.password,
+      body: Stack(
+        children: [
+          FlutterLogin(
+            theme: LoginTheme(
+              primaryColor: Colors.red,
+              accentColor: Colors.red[600],
+              buttonStyle: TextStyle(
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+              ),
+              errorColor: Colors.deepOrange,
+              titleStyle: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 48,
+                color: Colors.white,
+                letterSpacing: 2,
+              ),
             ),
-          );
-        },
-        emailValidator: (value) {
-          return value.isEmpty ? 'Email inserita non valida' : null;
-        },
-        passwordValidator: (value) {
-          return value.isEmpty ? 'Password inserita non valida' : null;
-        },
-        onSubmitAnimationCompleted: () {
-          switch (AppState().getRole()) {
-            case Role.DONOR:
-              Navigator.of(context).pushReplacementNamed("/donor");
-              break;
-            case Role.OFFICE:
-              Navigator.of(context).pushReplacementNamed("/office");
-              break;
-            case Role.ADMIN:
-              Navigator.of(context).pushReplacementNamed("/admin");
-              break;
-          }
-        },
-        messages: msg(),
-        onRecoverPassword: (_) {},
+            title: 'AnAVIS',
+            onLogin: _authUser,
+            logo: 'assets/images/homepage_login.png',
+            onSignup: (LoginData loginData) {
+              Navigator.of(context).pushReplacementNamed(
+                '/guest/createuser',
+                arguments: GuestCreateDonorArgs(
+                  loginData.name,
+                  loginData.password,
+                ),
+              );
+            },
+            emailValidator: (value) {
+              return value.isEmpty ? 'Email inserita non valida' : null;
+            },
+            passwordValidator: (value) {
+              return value.isEmpty ? 'Password inserita non valida' : null;
+            },
+            onSubmitAnimationCompleted: () {
+              switch (AppState().getRole()) {
+                case Role.DONOR:
+                  Navigator.of(context).pushReplacementNamed("/donor");
+                  break;
+                case Role.OFFICE:
+                  Navigator.of(context).pushReplacementNamed("/office");
+                  break;
+                case Role.ADMIN:
+                  Navigator.of(context).pushReplacementNamed("/admin");
+                  break;
+              }
+            },
+            messages: msg(),
+            onRecoverPassword: (_) {},
+          ),
+          Positioned(
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton.icon(
+                      icon: Icon(
+                        Icons.chat,
+                        color: Colors.red,
+                        size: 22,
+                      ),
+                      label: Text(
+                        "F.A.Q",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                      onPressed: () {
+                        return showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('F.A.Q'),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(8.0),
+                                ),
+                              ),
+                              content: const Text('Domanda e risposta'),
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text(
+                                    'Chiudi',
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      color: Colors.white,
+                      elevation: 8,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    RaisedButton.icon(
+                      icon: Icon(
+                        Icons.screen_lock_portrait,
+                        color: Colors.red,
+                        size: 22,
+                      ),
+                      label: Text(
+                        "Privacy",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                      onPressed: () {
+                        return showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Privacy'),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(8.0),
+                                ),
+                              ),
+                              content: const Text('Informativa'),
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text(
+                                    'Ho capito',
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      color: Colors.white,
+                      elevation: 8,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              left: 16,
+              right: 16,
+              bottom: 16),
+        ],
       ),
     );
   }
