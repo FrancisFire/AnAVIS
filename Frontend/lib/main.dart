4e34e3e0d2b20db80617abddb750bba348244005
azzeccagarbugli <f.coppola1998@gmail.com>
2020-01-29 17:10:25 +0100

    [UPDATE] Inserito IP relativo alla VPS per il prossimo deploy

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-const String ip = "192.168.43.169";
+const String ip = "46.101.201.248";
+//  IP VPS: 46.101.201.248
 
 void main() {
   initializeDateFormatting().then((_) => runApp(AnAvis()));
